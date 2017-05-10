8afb216dc38ab8e67e700601d86885a68e62b79a
MingdaRui <ruimingda@163.com>
2017-05-11 00:33:19 +0100

    Printing Out Numbers Recursively

--- src/chapter1/PrintingOutNumbers.java
+++ src/chapter1/PrintingOutNumbers.java
@@ -2,4 +2,25 @@ package chapter1;
 
 public class PrintingOutNumbers {
 
+	public static void main( String[] args ) {
+		
+		new PrintingOutNumbers().printOut( 735 );
+		
+	}
+	
+	public void printOut( int n ) { /* Print nonnegative n */
+		
+		if( n >= 10 ) {
+			printOut( n / 10 );
+		}
+		printDigit( n % 10 ); /* % is expensive */
+		
+	}
+	
+	public void printDigit( int n ) {
+		
+		System.out.print( n + " " );
+		
+	}
+	
 }
